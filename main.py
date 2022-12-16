465f565b1a6494b94ab1e02ae1a1c9fb055286f5
MMMatth <mat.loup04@gmail.com>
2022-12-16 21:32:09 +0100

    finito

--- main.py
+++ main.py
@@ -3,7 +3,24 @@ from openpyxl import load_workbook, Workbook
 import os , requests, shutil
 
 ## function ##
-# Fontion qui recupère les liens pdf sur le site
+# Fonction qui permet de modifier une case d'un fichier excel
+def modif_cellule(fichier, feuille, ligne, colonne, valeur):
+    """
+    modif_cellule est une fonction qui permet de modifier une case d'un fichier excel
+
+    Args:
+        fichier (str): lien du fichier excel
+        feuille (str): nom de la feuille
+        ligne (int): numéro de la ligne
+        colonne (int): numéro de la colonne
+        valeur (_type_): valeur à mettre dans la case
+    """
+    wb = load_workbook(fichier)
+    ws = wb[feuille]
+    ws.cell(row=ligne, column=colonne).value = valeur
+    wb.save(fichier)
+
+## class ##
 class pdf:
     def __init__(self, lien : str, nom : str):
         self.nom = nom # nom du prof
@@ -14,18 +31,11 @@ class pdf:
         self.recup_pdf()
     
     def __str__(self):
-        """
-        __str__ permet de définir ce que l'on veut afficher quand on affiche l'objet
-
-        Returns:
-            str : le lien du site du prof
-        """
+        """__str__ permet de définir ce que l'on veut afficher quand on affiche l'objet"""
         return self.lien
     
     def recup_pdf(self):
-        """
-        recup_pdf permet de récupérer les liens pdf sur le site du prof
-        """
+        """recup_pdf permet de récupérer les liens pdf sur le site du prof"""
         for i in range(len(self.page.text)):
             if self.page.text[i:i+4] == ".pdf": # si on trouve ".pdf"
                 for j in range (i, 0, -1): # on remonte dans la page jusqu'à trouver le début du lien
@@ -33,35 +43,30 @@ class pdf:
                         self.pdf.append(self.page.text[j+1:i+4]) # on ajoute le lien à la liste
                         break
     
-    def verif_url(self, url):
+    def verif_url(self, url : str):
+        """verif_url permet de verifier si le lien est bien une url et pas un lien relatif"""
+        return url[0:4] == "http"
+
+    
+    def dl_pdf(self, path : str):
         """
-        verif_url permet de verifier si le lien est bien une url et pas un lien relatif
+        dl_pdf est une fonction qui permet de télécharger les pdf dans un dossier
 
         Args:
-            url (str): lien à vérifier
-
-        Returns:
-            booléen : True si c'est une url, False sinon
+            path (str): chemin du dossier où télécharger les pdf
         """
-        if url[0:4] != "http":
-            return False
-        return True
-        
-    
-    def dl_pdf(self, path):
-        for i_lien in range(2):
-            if self.verif_url(self.pdf[i_lien]):
-                r = requests.get(self.pdf[i_lien], stream=True)
-                with open(path + str(i_lien) + ".pdf", 'wb') as f:
-                    f.write(r.content)
-                self.nbr_dl += 1
+        for i_lien in range(len(self.pdf)): 
+            if self.verif_url(self.pdf[i_lien]): # si c'est une url et pas un lien relatif
+                r = requests.get(self.pdf[i_lien], stream=True) # on récupère le pdf
+                with open(path + str(i_lien) + ".pdf", 'wb') as f: # on le télécharge dans le dossier
+                    f.write(r.content) # on écrit le contenu du pdf dans le fichier 
+                self.nbr_dl += 1 # on incrémente le nombre de pdf téléchargé
                 
     
       
          
 
 ## main ##
-
 ## lire le fichier excel et mettre les données dans un dictionnaire ##
 wb = load_workbook(filename = 'liste_ens.xlsx')
 sheet_ranges = wb['Feuil1']
@@ -71,22 +76,19 @@ site = {
     sheet_ranges['B4'].value: [sheet_ranges['C4'].value, None],
 }
 
-## on suprime les anciens fichiers ##
-for prenom in site.keys():
-    shutil.rmtree(prenom)
+# ## on suprime les anciens fichiers ##
+# for prenom in site.keys():
+#     shutil.rmtree(prenom)
 
 ## crée un dossier par professeur ##
-for prenom in site.keys():
+for prenom in site.keys(): 
     os.mkdir(prenom)
     
 ## on récupère les liens des pdf sur les sites ##
-indice = 1
+l_table = 1 
 for prenom in site.keys():
-    site[prenom][1] = pdf(site[prenom][0],prenom)
-    site[prenom][1].dl_pdf(str(prenom)+"/"+str(prenom))
-    # modifie le fichier excel liste_ens.xlsx
-    wb = load_workbook(filename = 'liste_ens.xlsx')
-    sheet_ranges = wb['Feuil1']
-    sheet_ranges.cell("D"+str(indice)).value = site[prenom][1].nbr_dl
-    indice += 1
+    site[prenom][1] = pdf(site[prenom][0],prenom) # on crée un objet pdf pour chaque prof
+    site[prenom][1].dl_pdf(str(prenom)+"/"+str(prenom)) # on télécharge les pdf dans le dossier de chaque prof
+    modif_cellule("liste_ens.xlsx", "Feuil1", l_table + 1, 4, site[prenom][1].nbr_dl) # on modifie le fichier excel
+    l_table += 1 # on incrémente l_table pour aller à la ligne suivante du fichier excel
     
